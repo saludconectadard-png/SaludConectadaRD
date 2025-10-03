2b2ec30fb840cde7ea0ce1c6354b0091058738ea
saludconectadard-png <saludconectadard@gmail.com>
2025-10-03 11:24:08 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,16 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   poweredByHeader: false,
   compress: true,
-
   images: {
     formats: ['image/webp', 'image/avif'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
-
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+    },
+  },
+  output: 'standalone',
   async headers() {
     return [
       {
